127ac93bffa40b23b42b4df1ab290e35f0f6788e
ryrathb <107002381+ryrathb@users.noreply.github.com>
2023-10-25 12:04:24 -0700

    Update Execute.v

--- source-files/Execute.v
+++ source-files/Execute.v
@@ -22,25 +22,27 @@
 
 module Execute(
 RegDst, ALUSrc0, ALUSrc1, Shamt,
-Reg_Data1, Reg_Data2, Imm32b, PCPlusFour,
+Reg_Data1, Reg_Data2, Imm32b, PCPlusFour, Instruction, Opcode, instr_index, rt, rd,
 
-Zero, rDestSelected, ALUResult,
+Zero, ALUResult,
 j_sll_two, PC_Plus_Branch
 );
 
 input ALUSrc0, RegDst;
 input [1:0] ALUSrc1;
-input [4:0] Shamt;
+input [4:0] Shamt, rt, rd;
 input [31:0] Reg_Data1, Reg_Data2, Imm32b, PCPlusFour;
-
+input [5:0] Instruction, Opcode;
+input [25:0] instr_index;
 
 wire [3:0] ALUControl;
 wire [31:0] ALU_A, ALU_B;
+wire [31:0] Imm32b_sll_two;
 
 output reg Zero;
-output reg [4:0] rDestSelected;
 output reg [31:0] ALUResult;
-output reg [31:0] j_sll_two, PC_Plus_Branch;
+output reg [31:0] PC_Plus_Branch;
+output reg [27:0] j_sll_two;
 
 Mux32bit2to1 ALU_A_Mux(Reg_Data1, Shamt, ALU_A, ALUSrc0);
 Mux32bit4to1 ALU_B_Mux(Reg_Data2, Imm32b, 8, 10, ALU_B, ALUSrc1);
@@ -49,5 +51,6 @@ ALU Sys_ALU(ALUControl, ALU_A, ALU_B, ALUResult, Zero);
 LeftShift2 SLL_j(instr_index, j_sll_two);
 LeftShift2 SLL_Imm(Imm32b, Imm32b_sll_two);
 Add Branch_Adder(PCPlusFour, Imm32b_sll_two, PC_Plus_Branch);
-Mux32bit2to1 regDest_Mux(rt, rd, rDestSelected, RegDst);
+Mux32bit2to1 regDest_Mux(rt, rd, RegDestSelected, RegDst);
 endmodule
+
