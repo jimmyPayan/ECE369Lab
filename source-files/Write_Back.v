94c1474e1f298b9ffda74cfedd80aea39367401b
ryrathb <107002381+ryrathb@users.noreply.github.com>
2023-10-24 22:52:59 -0700

    Update Write_Back.v

--- source-files/Write_Back.v
+++ source-files/Write_Back.v
@@ -20,7 +20,20 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Write_Back(
+module Write_Back(RegWrite_In, MemToReg, R_Data, ALUResult, rDestSelected_in, RegWrite_Out, regWriteData, rDestSelected_Out);
+    input RegWrite_In, MemToReg;
+    input [31:0] ALUResult, R_Data;
+    input [4:0] rDestSelected_in;
+    output reg RegWrite_Out;
+    output reg rDestSelected_Out;
+    output reg [31:0] regWriteData;
 
-    );
+    always @(*) begin
+        RegWrite_Out <= RegWrite_In;
+        rDestSelected_Out <= rDestSelected_in;
+    end 
+
+    Mux32bit2to1 mux(R_Data, ALUResult, regWriteData, MemToReg);
+    
+
 endmodule
