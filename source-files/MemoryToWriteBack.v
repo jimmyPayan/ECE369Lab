940a5f27885a4a4f0de99513b4b1e084cdafda5f
ryrathb <107002381+ryrathb@users.noreply.github.com>
2023-10-24 22:52:24 -0700

    Update MemoryToWriteBack.v

--- source-files/MemoryToWriteBack.v
+++ source-files/MemoryToWriteBack.v
@@ -22,16 +22,24 @@
 
 module MemoryToWriteBack
 (Clock, 
-RegWriteIn, MemToRegIn, 
-RegWriteOut, MemToRegOut
+RegWriteIn, MemToRegIn, R_Data_In, ALUResult_In, rDestSelected_in,
+RegWriteOut, MemToRegOut, R_Data_Out, ALUResult_Out, rDestSelected_Out
 );
 input Clock;
 input RegWriteIn, MemToRegIn;
+input [31:0] ALUResult_In, R_Data_In;
+input [4:0] rDestSelected_in;
+
 output reg RegWriteOut, MemToRegOut;
+output reg [31:0] ALUResult_Out, R_Data_Out;
+output reg [4:0] rDestSelected_Out;
 
 always @ (posedge Clock) begin 
 RegWriteOut <= RegWriteIn;
 MemToRegOut <= MemToRegIn;
+ALUResult_Out <= ALUResult_In;
+R_Data_Out <= R_Data_In;
+rDestSelected_Out <= rDestSelected_in;
 end
 
 endmodule
