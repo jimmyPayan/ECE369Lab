af82b63b8d285e105b2b0b1272302ff99461d1d4
ryrathb <107002381+ryrathb@users.noreply.github.com>
2023-10-24 22:51:26 -0700

    Update Memory.v

--- source-files/Memory.v
+++ source-files/Memory.v
@@ -20,7 +20,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Memory(
+module Memory(R_Enable, W_Enable, R_Width, W_Width, BranchSel, PC_Plus_Branch, Zero, ALUResult, Reg_Data2, j_sll_two, Clock);
+    input R_Enable, W_Enable, Zero, Clock;
+    input [1:0] R_Width, W_Width;
+    input [3:0] BranchSel;
+    input [31:0] PC_Plus_Branch, ALUResult, Reg_Data2;
+    input [27:0] j_sll_two;
 
-    );
+    output reg [31:0] R_Data, PCNew;
+    output reg [1:0] PCSrc;
+
+    DataMemory dataMem(Clock, W_Enable, R_Enable, ALUResult, R_Data, Reg_Data2, R_Width, W_Width);
+    PCSrc_Control pcsrccont(BranchSel, Zero, ALUResult, j_sll_two, PC_Plus_Branch, PCSrc, PCNew);
+    
+
+
 endmodule
