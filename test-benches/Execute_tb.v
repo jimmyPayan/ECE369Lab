a9849142404225617eb5212452b32db5d7af6ba9
jimmyPayan <117542462+jimmyPayan@users.noreply.github.com>
2023-10-27 11:06:19 -0700

    made execute tb & added file for fetch&decode stages

--- test-benches/Execute_tb.v
+++ test-benches/Execute_tb.v
@@ -23,8 +23,9 @@
 module Execute_tb();
 reg [4:0] RegDst, rt, rd;
 reg [31:0] ALUSrc0, ALUSrc1,Imm32b, PCPlusFour;
-reg [4:0] Shamt, Reg_Data1, Reg_Data2, Opcode;
-reg [5:0] Instruction;
+reg [4:0] Shamt;
+reg [31:0] Reg_Data1, Reg_Data2;
+reg [5:0] Instruction, Opcode;
 reg [25:0] instr_index;
 
 wire Zero_output;
@@ -44,10 +45,21 @@ j_sll_two_output, PC_Plus_Branch_output, RegDestSelected_output
 initial begin
 
 // add $16, $8, $12
-RegDst <= 0;
+RegDst <= 1; // rDestSelected should be 16
 ALUSrc0 <= 0;
-ALUSrc1 <= 1;
+ALUSrc1 <= 0;
 Shamt <= 16;
+Reg_Data1 <= 108;
+Reg_Data2 <= 112;
+Imm32b <= 15;   // PC Plus Branch should be 64
+PCPlusFour <= 4;
+Instruction <= 6'b000000;
+Opcode <= 6'b100000; // ALUResult should be 120
+instr_index <= {2'b00, 24'hFFFFFF}; // Result should be 3FFFFFC
+rt <= 5'bXXXXXX;
+rd <= 16;
+#100;
+$finish;
 end
 
 endmodule
---
+++ test-benches/FetchAndDecode_Stages_tb.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/27/2023 11:03:52 AM
+// Design Name: 
+// Module Name: FetchAndDecode_Stages_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FetchAndDecode_Stages_tb();
+endmodule
