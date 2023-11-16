7910fb3e96aecad8ad95d8e4ef31266fcefe600f
ryrathb <107002381+ryrathb@users.noreply.github.com>
2023-11-16 14:11:41 -0700

    Update HazardDetection.v

--- source-files/HazardDetection.v
+++ source-files/HazardDetection.v
@@ -22,28 +22,47 @@
 
 module HazardDetection(
 // Inputs
-rs, rt, opcode, rDestSelected,
+ID_rs, ID_rt, EX_opcode, MEM_opcode, MEM_rDestSelected, EX_rt, EX_rd,
 
 // Outputs
-stall
+Stall_ID, Stall_PC, Stall_ID_EX
 );
 
-input [4:0] rs, rt, rDestSelected;
-input [5:0] opcode;
+input [4:0] ID_rs, ID_rt, MEM_rDestSelected, EX_rt, EX_rd;
+input [5:0] EX_opcode, MEM_opcode;
 
-output reg stall;
+output reg Stall_ID, Stall_PC, Stall_ID_EX;
 
 always @ (*) begin
-
-// if opcode == R type && ( rDestSelected == rs || rDestSelected == rt ) && (rDestSelected != 0)
-//   stall 1
-
-// else if opcode == I type && rDestSelected == rs
-//   stall 1
-
-// if (branch)
-//   flush 1 - is this right?
-
+    // R-Type dependency between an instruction in ID and an instruction in EX
+    // Last is condition is necessary so the pipeline doesn't stall when 2 nop's are in a row or when instructions like bgtz (rt == 0) have a no op in front
+    if ((((ID_rs == EX_rd) && (ID_rs != 0)) || ((ID_rt == EX_rd) && (ID_rt != 0))) && (EX_opcode == 6'b000000 || EX_opcode == 6'b011100)) begin
+        Stall_ID <= 1;
+        Stall_PC <= 1;
+        Stall_ID_EX <= 1;
+    end 
+    
+    // I-Type dependency between and instruction in ID and an instruction in EX
+    else if (((ID_rs == EX_rt && ID_rs != 0) || (ID_rt == EX_rt && ID_rt != 0)) && (EX_opcode != 6'b000000)) begin 
+        Stall_ID <= 1;
+        Stall_PC <= 1;
+        Stall_ID_EX <= 1;
+    end
+    
+    // R-Type or I-Type dependency between an instruction in ID and an instruction in MEM
+    else if ((ID_rs == MEM_rDestSelected || ID_rt == MEM_rDestSelected) && (MEM_rDestSelected != 0) && (ID_rs != 0 && ID_rt != 0)) begin
+        Stall_ID <= 1; 
+        Stall_PC <= 1;
+        Stall_ID_EX <= 1;
+    end
+    
+    // No dependency exists
+    else begin 
+        Stall_ID <= 0;
+        Stall_PC <= 0;
+        Stall_ID_EX <= 0;
+    end
+    
 end
 
 endmodule
